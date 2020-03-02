0c81e369b0f62055f9508d7a42655e92aa5cc9ab
Brian Butz <bbutz@pivotal.io>
2020-03-02 17:37:38 +0000

    Refactor controller to share setting of model attributes
    Signed-off-by: Gary MacCabe <gary.maccabe100@airm365.onmicrosoft.com>

--- src/main/java/com/example/rpsgame/WebController.java
+++ src/main/java/com/example/rpsgame/WebController.java
@@ -11,8 +11,10 @@ public class WebController {
     private Computer computer;
     private Cheater cheater;
 
-    public WebController(Computer computer, Cheater cheater) {
-
+    public WebController(
+            Computer computer,
+            Cheater cheater
+    ) {
         this.computer = computer;
         this.cheater = cheater;
     }
@@ -27,52 +29,38 @@ public class WebController {
             return "index";
         }
 
+        String computerChoice = null;
+
         switch (theOpponentType) {
-
             case "computer":
-
-                String computerRandomChoice = computer.getComputerChoice();
-                String theOutcome;
-
-                if (
-                    thePlayersChoice.equals("rock") && computerRandomChoice.equals("scissors") ||
-                    thePlayersChoice.equals("paper") && computerRandomChoice.equals("rock") ||
-                    thePlayersChoice.equals("scissors") && computerRandomChoice.equals("paper"))
-                {
-                    theOutcome = "You Win";
-                } else if (thePlayersChoice.equals(computerRandomChoice)) {
-                    theOutcome = "Draw";
-                } else {
-                    theOutcome = "Computer Wins";
-                }
-
-                model.addAttribute("opponentType", theOpponentType);
-                model.addAttribute("personChoice", thePlayersChoice);
-                model.addAttribute("computerChoice", computerRandomChoice);
-                model.addAttribute("outcome", theOutcome);
-
-                return "results";
+                computerChoice = computer.getComputerChoice();
+                break;
 
             case "cheater":
-
-                String computerCheats = cheater.getChoiceViaCheating(thePlayersChoice);
-
-                if (
-                    thePlayersChoice.equals("rock") ||
-                    thePlayersChoice.equals("paper") ||
-                    thePlayersChoice.equals("scissors"))
-                {
-                    cheater.getChoiceViaCheating(thePlayersChoice);
-                    theOutcome = "Computer Wins";
-
-                    model.addAttribute("opponentType", theOpponentType);
-                    model.addAttribute("personChoice", thePlayersChoice);
-                    model.addAttribute("computerChoice", computerCheats);
-                    model.addAttribute("outcome", theOutcome);
-                    return "results";
-                }
+                computerChoice = cheater.getChoiceViaCheating(thePlayersChoice);
+                break;
         }
-        return "index";
+
+        model.addAttribute("opponentType", theOpponentType);
+        model.addAttribute("personChoice", thePlayersChoice);
+        model.addAttribute("computerChoice", computerChoice);
+        model.addAttribute("outcome", playRPS(thePlayersChoice, computerChoice));
+
+        return "results";
+    }
+
+    private String playRPS(String thePlayersChoice, String computerChoice) {
+        if (
+                thePlayersChoice.equals("rock") && computerChoice.equals("scissors") ||
+                        thePlayersChoice.equals("paper") && computerChoice.equals("rock") ||
+                        thePlayersChoice.equals("scissors") && computerChoice.equals("paper"))
+        {
+            return "You Win";
+        } else if (thePlayersChoice.equals(computerChoice)) {
+            return "Draw";
+        } else {
+            return "Computer Wins";
+        }
     }
 }
 
